91b71c4a5b76c4bae7de71ccf5c60cb6cc1e3fb8
Oscar <oscar.kennedysmith@gmail.com>
2020-06-13 12:20:55 +1000

    Users page linked to User components

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+
 const Users = () => {
-  return <h2>Users Works!</h2>
+  const USERS = [{
+    id: 'u1',
+    name: 'Oscar Kennedy Smith',
+    image: "https://dashingdesignerdogs.com.au/wp-content/uploads/2019/04/Doggy-Day-Care-Mornington-3.jpg",
+    places: 3,
+  }
+];
+
+  return <UsersList items={USERS}/>
 };
 
 export default Users; 
