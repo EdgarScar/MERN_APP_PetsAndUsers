87b9f51af1fe2caa2caeb5d17d9eb750047c4398
Oscar <oscar.kennedysmith@gmail.com>
2020-06-26 11:45:38 +1000

    side drawer fixed

--- src/shared/components/Navigation/SideDrawer.js
+++ src/shared/components/Navigation/SideDrawer.js
@@ -9,15 +9,19 @@ import './SideDrawer.css';
 
 //Generates an SideDrawer pop out for mobile ratios 
 const SideDrawer = props => {
-  /** Can define variables as a JSX element/s
-        const content = <aside className='side-drawer'>{props.children}</aside>
-  **/
-  /**CSSTransition element wraps for animation
-   * 
-  **/
-    <CSSTransition in={props.show} timeout={200}>
-    const content = <aside className='side-drawer'>{props.children}</aside>
+  //Can define variables as a JSX element/s
+    const content = (
+  //CSSTransition element wraps for animation
+      <CSSTransition 
+      in={props.show} 
+      timeout={200} 
+      classNames="slide-in-left"
+      mountOnEnter
+      unmountOnExit
+      >  
+      <aside className='side-drawer'>{props.children}</aside>
     </CSSTransition>
+    );
   //Create a portal to render this JSX 'content' in a different DOM element to where 
   //  this parent component is called and rendered. 
   return ReactDOM.createPortal(content, document.getElementById('drawer-hook'));
